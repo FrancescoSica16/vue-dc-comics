9fb5a779a380f0adadc5dfdbd7782d17181e703c
FrancescoSica16 <francesco.sica16@gmail.com>
2021-09-24 17:04:29 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,42 @@
 <template>
   <header>
+    <a href="#" class="header-logo">
+      <img src="../assets/img/dc-logo.png" alt="">
+    </a>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Characters</a>
+        </li>
+        <li>
+          <a href="#" class="active">Comics</a>
+        </li>
+        <li>
+          <a href="#">Movies</a>
+        </li>
+        <li>
+          <a href="#">TV</a>
+        </li>
+        <li>
+          <a href="#">Games</a>
+        </li>
+          <li>
+          <a href="#">collectibles</a>
+        </li>
+        <li>
+          <a href="#">Video</a>
+        </li>
+        <li>
+          <a href="#">Fans</a>
+        </li>
+          <li>
+          <a href="#">News</a>
+        </li>
+        <li>
+          <a href="#">Shop</a>
+        </li>
+      </ul>
+    </nav>
 
   </header>
 </template>
@@ -14,5 +51,45 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "../style/variabiles.scss";
+
+header{
+  width: 1000px;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+}
+
+img{
+  height: 50px;
+}
+
+ul{
+  display: flex;
+  list-style: none;
+  text-decoration: none;
+}
+
+li a{
+  margin: 0px 15px;
+  padding-bottom: calc(100% - 45px);
+  
+  text-decoration: none;
+  font-family: 'Roboto';
+  font-weight: 500;
+  font-size: 0.7rem;
+  color: $navFontColor;
+  text-transform: uppercase;
+
+  &.active{
+    border-bottom: 4px solid rgb(12, 124, 236);
+    color:rgb(12, 124, 236) ;
+  }
+
+  &:hover{
+    color : darken(rgb(12, 124, 236), 5%);
+  }
+}
 
 </style>
