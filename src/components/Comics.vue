8196adeab964da4a60a8b7a399a5b33fc29055c1
FrancescoSica16 <francesco.sica16@gmail.com>
2021-09-27 15:34:30 +0200

    struttura comic item

---
+++ src/components/Comics.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="comic-item">
+        <h2>Content main section</h2>
+      </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .comic-item{
+        flex-basis: calc(100% / 6 - 18px); // da chiarire il funzionamento
+        height: 200px;
+        background-color: blueviolet;
+        border: 1px solid black;
+        margin: 3px;
+    }
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="main-block">
+    <div id="main-wrapper" class="container">
+    <Comics/>
+    <Comics/>
+    <Comics/>
+    <Comics/>
+    <Comics/>
+    <Comics/>
+    <Comics/>
+    <Comics/>
+    <Comics/>
+    <Comics/>
+    <Comics/>
+    <Comics/>
+
+    
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Comics from "./Comics.vue";
+
+export default {
+  name: 'Products',
+  components: {
+    Comics,
+  }
+  
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../style/variabiles.scss";
+  #main-block{
+    background-color: black;
+  }
+  #main-wrapper{
+    background-color: black;
+    display: flex;
+    flex-wrap: wrap;
+    
+    margin: 0 auto;
+
+    & h2{
+      padding: 50px 0px;
+      color: white;
+      font-family: $mainFont;
+      
+    }
+  }
+
+  
+
+</style>
